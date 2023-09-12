68ba725bb4db6435b3b25568d7b8ca403e068087
Duong BKT <duongbkt332001@gmail.com>
2023-09-12 16:15:11 +0700

    add trim

--- KoaJs/src/routes/routes.js
+++ KoaJs/src/routes/routes.js
@@ -2,6 +2,7 @@ const Router = require("koa-router");
 const productHandler = require("../handlers/products/productHandlers");
 const todoHandler = require("../handlers/todos/todoHandlers");
 const productInputMiddleware = require("../middleware/productInputMiddleware");
+const todoInputMiddleware = require("../middleware/todoInputMiddleware");
 
 const router = new Router({
   prefix: "/api",
@@ -17,7 +18,7 @@ router.delete("/product/:id", productHandler.removeProduct);
 // Routes todos
 router.get("/todos", todoHandler.getTodos);
 router.put("/todo/:id", todoHandler.update);
-router.post("/todos", todoHandler.save);
+router.post("/todos", todoInputMiddleware, todoHandler.save);
 router.post("/todo", todoHandler.removeManyTodo);
 router.delete("/todo/:id", todoHandler.removeTodo);
 router.put("/todo", todoHandler.updateManyTodo);

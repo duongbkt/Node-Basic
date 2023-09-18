b2a7b0ae232bd3b519065a8f0f7dd6af660f46d3
Duong BKT <duongbkt332001@gmail.com>
2023-09-18 16:26:16 +0700

    fix cors

--- KoaJs/src/app.js
+++ KoaJs/src/app.js
@@ -7,7 +7,7 @@ const app = new Koa();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000" | "http://localhost:3001",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
